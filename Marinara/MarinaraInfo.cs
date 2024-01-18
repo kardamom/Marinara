eb49040c3a0f607da7ac7e60dc16e1de55aa7be7
David Wright <dave-git@dexy.org>
2024-01-18 00:08:49 -0800

    Version numbers

--- Marinara/MarinaraInfo.cs
+++ Marinara/MarinaraInfo.cs
@@ -3,11 +3,11 @@ using Grasshopper.Kernel;
 using System;
 using System.Drawing;
 
-namespace Marinara3
+namespace Marinara
 {
     public class MarinaraInfo : GH_AssemblyInfo
     {
-        public override string Name => "Marinara3";
+        public override string Name => "Marinara";
 
         //Return a 24x24 pixel bitmap to represent this GHA library.
         public override Bitmap Icon => null;
@@ -18,9 +18,9 @@ namespace Marinara3
         public override Guid Id => new Guid("818b80e4-76a4-4ffe-a7e0-ced41dfdb3c6");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "David Wright";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "kardamom@gmail.com";
     }
 }
